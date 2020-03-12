6f6105cd1ff81e30b6b74af9f71c5582c304b380
pranavmswamy <pranavmpv@gmail.com>
2020-03-12 15:21:55 -0700

    Updated

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
 from pyspark import SparkContext
+from itertools import combinations
 
 sc = SparkContext()
 
@@ -48,8 +49,6 @@ def lsh(sig_key_column):
     generate (b_num, hash_val), b_id) for each set of rows in a band.
     group by key
     return ((band_num, hashvalue), (b_id))
-    :param sig_column:
-    :return:
     '''
     c = 4294967539
 
@@ -57,9 +56,8 @@ def lsh(sig_key_column):
     band_hash_list = []
     sig_column = sig_key_column[1]
 
-    counter = 0
     b = 0
-    for counter in range(0,len(sig_column),r):
+    for counter in range(0, len(sig_column), r):
         left_idx = counter
         right_idx = counter + r
         b += 1
@@ -67,18 +65,34 @@ def lsh(sig_key_column):
         combined_str = ""
         for num in range(left_idx, right_idx):
             combined_str = combined_str + str(sig_column[num])
-        band_row_hash = (h1(int(combined_str)) + b * h2(int(combined_str))) % c
+        band_row_hash = (h1(int(combined_str)) + b * h2(int(combined_str))) % (100 // r)
+        # band_row_hash = int(combined_str) % (100 // r)
         band_hash_list.append((str(b)+ str(band_row_hash), sig_key_column[0]))
         # returns list of tuples
 
     return band_hash_list
+
+
+def find_similarity(couple):
+    shingles1 = set(users_by_business_id[couple[0]])
+    shingles2 = set(users_by_business_id[couple[1]])
+    similarity = len(shingles1.intersection(shingles2)) / len(shingles1.union(shingles2))
+    # if similarity >= 0.5:
+    return (couple[0], couple[1], similarity)
+    # else:
+        # return ('<0.5')
+
 
 # -----------------------------
 
 rdd = sc.textFile("yelp_train.csv").filter(lambda s: s.startswith('user_id') is False) \
     .map(lambda s: tuple(s.split(",")[-2::-1])) # business_id, user_id
 
-rdd_by_business = rdd.groupByKey().mapValues(hash_to_32_bit_nums)
+rdd_by_business_id = rdd.groupByKey().persist()
+
+users_by_business_id = dict(rdd_by_business_id.collect())
+
+rdd_by_business = rdd_by_business_id.mapValues(hash_to_32_bit_nums)
 
 sig_matrix_rdd = rdd_by_business.mapValues(convert_to_sig_matrix)
 
@@ -88,9 +102,27 @@ lsh_rdd = sig_matrix_rdd.map(lsh)
 lsh_rdd = lsh_rdd.flatMap(lambda list: list).groupByKey().mapValues(lambda l: list(l))
 lsh_rdd = lsh_rdd.filter(lambda x: len(x[1]) > 1)
 
-sim_items_rdd = lsh_rdd.map(lambda x: tuple(sorted(x[1]))).distinct()
+sim_items_candidates_rdd = lsh_rdd.map(lambda x: tuple(sorted(x[1]))).distinct()
 
-a = sim_items_rdd.collect()
+sim_items_candidates_rdd = sim_items_candidates_rdd.flatMap(lambda items: combinations(items,2)).map(lambda couple: tuple(sorted(couple))).distinct()
 
-for i in a:
-    print("\n",i)
+sim_items_candidates_rdd = sim_items_candidates_rdd.map(find_similarity)
+
+similar_candidates = sim_items_candidates_rdd.collect()
+
+
+for item in similar_candidates:
+    print(item)
+print(len(similar_candidates))
+'''
+similar_items_list = []
+for candidate_set in similar_candidates:
+    potential_sim_items = list(combinations(candidate_set, 2))
+    for couple in potential_sim_items:
+        similarity = find_similarity(couple, rdd_by_business_id)
+        if similarity >= 0.5:
+            similar_items_list.append((couple[0], couple[1], similarity))
+
+print(similar_items_list)'''
+
+
